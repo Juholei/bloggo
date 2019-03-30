88edccadefa67e6e92cf0a946bc19f245f86832d
Juho Leinonen <juholei@gmail.com>
2019-03-30 10:36:12 +0200

    Add backend route for retrieving single full post, make posts route return only previews of the posts

--- src/lambda/bloggo/lambda/core.cljs
+++ src/lambda/bloggo/lambda/core.cljs
@@ -1,12 +1,11 @@
 (ns bloggo.lambda.core
-  (:require [bloggo.lambda.util :as util]))
+  (:require [bloggo.lambda.util :as util]
+            [clojure.string :as str]))
 
-(defn posts [e ctx cb]
-  (println (-> e util/clojurify-event :path-parameters))
-  (cb nil (clj->js {:statusCode 200
-                    :body (-> [{:post "## Hello \n * jee \n * woah"
-                                :title "This is a blog post"}
-                               {:post "Quis optio quibusdam recusandae. Sed tempora vel natus necessitatibus aut nostrum. Eligendi qui non molestias est voluptatem molestias quos porro. Laborum veritatis minima ut voluptates molestiae aut occaecati quidem. Possimus nisi voluptas id amet quasi officiis quia. Cumque nihil laborum sint.
+(def mock-posts [{:post "## Hello \n * jee \n * woah"
+                  :title "This is a blog post"
+                  :id 0}
+                 {:post "Quis optio quibusdam recusandae. Sed tempora vel natus necessitatibus aut nostrum. Eligendi qui non molestias est voluptatem molestias quos porro. Laborum veritatis minima ut voluptates molestiae aut occaecati quidem. Possimus nisi voluptas id amet quasi officiis quia. Cumque nihil laborum sint.
 
 Et nesciunt eligendi itaque voluptas asperiores nihil quis ea. Harum cumque harum non qui cumque et illo quod. Dignissimos ut placeat iste placeat amet. Consequatur voluptatibus voluptatem quisquam.
 
@@ -15,9 +14,33 @@ Est excepturi iure nesciunt esse possimus provident quae. Reprehenderit temporib
 Corrupti eaque nihil et ex doloribus architecto officia iusto. Doloribus dolores aspernatur soluta aspernatur. Voluptatem corporis ipsum necessitatibus magnam quia temporibus totam. Tempora ex id qui ex quo est doloribus. Consectetur id minima qui ea ut dolore.
 
 Ipsa est harum et consequatur temporibus qui. Ut optio non nisi rem voluptatem. Voluptate debitis qui culpa temporibus. Beatae atque sit mollitia vitae sunt. Optio sit animi aut."
-                                :title "This is another blog post"}]
+                  :title "This is another blog post"
+                  :id 1}])
+
+(defn post->preview [post]
+  (update post :post (fn [post-content]
+                       (-> post-content
+                           (str/split #"\n")
+                           first))))
+
+(defn posts->previews [posts]
+  (map post->preview posts))
+
+(defn posts [e ctx cb]
+  (println (-> e util/clojurify-event :path-parameters))
+  (cb nil (clj->js {:statusCode 200
+                    :body (-> mock-posts
+                              posts->previews
                               clj->js
                               js/JSON.stringify)})))
+
+(defn post [e ctx cb]
+  (let [post-id (-> e util/clojurify-event :path-parameters :id int)]
+    (cb nil (clj->js {:statusCode 200
+                      :body (-> mock-posts
+                                (nth post-id)
+                                clj->js
+                                js/JSON.stringify)}))))
 
 
 
