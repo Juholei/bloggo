70af429cb24bccca62d1c37ab013600888236267
Juho Leinonen <juholei@gmail.com>
2019-05-04 17:28:30 +0300

    Set the initial view in app-state to be the frontpage

--- src/app/bloggo/main/state.cljs
+++ src/app/bloggo/main/state.cljs
@@ -1,5 +1,6 @@
 (ns bloggo.main.state
   (:require [reagent.core :as r]))
 
-(defonce app-state (r/atom {:new-post "- This is content"}))
+(defonce app-state (r/atom {:new-post "- This is content"
+                            :view :bloggo.main.routes/frontpage}))
 
