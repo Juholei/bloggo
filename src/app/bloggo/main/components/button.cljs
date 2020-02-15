628948bbcd5cbc5e4a933eb816b571d4f7c2f51d
Juho Leinonen <juholei@gmail.com>
2020-02-15 16:23:43 +0200

    Some styles for button

--- src/app/bloggo/main/components/button.cljs
+++ src/app/bloggo/main/components/button.cljs
@@ -1,6 +1,15 @@
 (ns bloggo.main.components.button)
 
 (defn button [text on-click]
-  [:button {:type :button
+  [:button {:class ["bg-blue-500"
+                    "hover:bg-blue-700"
+                    "text-white"
+                    "font-bold"
+                    "py-2"
+                    "px-4"
+                    "rounded"
+                    "my-2"
+                    "mx-4"]
+            :type :button
             :on-click on-click}
    text])
