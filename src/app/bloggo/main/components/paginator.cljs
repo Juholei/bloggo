a0ff396f255cd0147adb7d49605f4ad2dcd9af16
Juho Leinonen <juholei@gmail.com>
2020-02-22 17:43:52 +0200

    Some styles for paginator

--- src/app/bloggo/main/components/paginator.cljs
+++ src/app/bloggo/main/components/paginator.cljs
@@ -1,13 +1,14 @@
 (ns bloggo.main.components.paginator)
 
 (defn page-link [current-page? text destination]
-  [:a {:href destination}
+  [:a {:class "bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4"
+       :href destination}
    (if current-page?
      [:b text]
      text)])
 
 (defn paginator [current-page page-count url-fn]
-  [:div
+  [:div.inline-flex
    (for [page-num (range 1 (inc page-count))
          :let [current-page? (= current-page page-num)]]
      ^{:key page-num}
