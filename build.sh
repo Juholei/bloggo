808fb456ce19ef74af829660a35d39827e512abd
Juho Leinonen <juholei@gmail.com>
2020-11-06 17:39:45 +0200

    remove all lambda stuff, at least for now

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-shadow-cljs release app
-shadow-cljs release lambda
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-serverless deploy
-serverless client deploy --no-confirm --no-config-change --no-policy-change --no-cors-change
