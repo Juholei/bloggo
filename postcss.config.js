08fd1b1aa0f7dfae0a9cb60fc2e26b1f1b646db5
Juho Leinonen <juholei@gmail.com>
2020-11-22 16:50:31 +0200

    Upgrade tailwindcss to v2, use tailwind config to purge css instead of directly using purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,3 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project 
-  content: [
-    './src/**/*.cljs',
-    './public/index.html'
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
-
 const cssnano = require('cssnano')({
   preset: 'default',
 });
@@ -19,7 +7,7 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     ...process.env.NODE_ENV === 'production'
-      ? [purgecss, cssnano]
+      ? [cssnano]
       : []
   ]
 }
